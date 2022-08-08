033d33dcb131e905649c408357064b9b6fb4fd5c
karmakettle <karen@blacklocus.com>
2022-08-07 19:33:57 -0500

    Set up two endpoints

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,12 +7,52 @@ import (
   "os"
 )
 
-func getTest(w http.ResponseWriter, r *http.Request) {
-  // ctx := r.Context()
+/*
+  Requires POST, topic
+*/
+func subscribe(w http.ResponseWriter, r *http.Request) {
+  if r.Method != http.MethodPost {
+    err := r.Method + " not allowed for " + "/subscribe"
+    fmt.Println(err)
+    http.Error(w, err,  http.StatusMethodNotAllowed)
+    return
+  }
 
   topic := r.URL.Query().Get("topic")
+  if !(len(topic) > 0) {
+    err := "No topic specified, unable to subscribe"
+    fmt.Println(err)
+    http.Error(w, err,  http.StatusBadRequest)
+    return
+  }
 
-  fmt.Printf("%s /test - topic %s\n", r.Method, topic)
+  fmt.Printf("%s /subscribe - topic %s\n", r.Method, topic)
+}
+
+/*
+  Requires POST, topic, JSON body
+*/
+func publish(w http.ResponseWriter, r *http.Request) {
+  // websocket connection should already be established via /subscribe
+
+  if r.Method != http.MethodPost {
+    err := r.Method + " not allowed for " + "/publish"
+    fmt.Println(err)
+    http.Error(w, err,  http.StatusMethodNotAllowed)
+    return
+  }
+
+  topic := r.URL.Query().Get("topic")
+  if !(len(topic) > 0) {
+    err := "No topic specified, unable to publish"
+    fmt.Println(err)
+    http.Error(w, err, http.StatusBadRequest)
+    return
+  }
+
+  // TODO require JSON body
+
+  fmt.Printf("%s /publish - topic %s\n", r.Method, topic)
 }
 
 func main() {
@@ -21,7 +61,8 @@ func main() {
   flag.Parse()
 
   mux := http.NewServeMux()
-  mux.HandleFunc("/test", getTest)
+  mux.HandleFunc("/publish", publish)
+  mux.HandleFunc("/subscribe", subscribe)
 
   fmt.Printf("Starting server on port %s\n", port)
   err := http.ListenAndServe(":" + port, mux)
@@ -31,3 +72,4 @@ func main() {
     os.Exit(1)
   }
 }
+
