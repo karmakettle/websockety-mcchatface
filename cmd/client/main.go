a1b256862c4c5b31ef45dfb158f2e70b19c046cf
karmakettle <karen@blacklocus.com>
2022-08-09 04:03:05 -0500

    Apply go fmt

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,65 +4,65 @@
 //
 // Usage:
 //
-//   go run cmd/client/main.go [flags]
+//	go run cmd/client/main.go [flags]
 //
 // Flags:
 //
-//  -topic
-//    Topic to receive incoming messages from (required).
-//  -port
-//    Port the server is running on (optional, defaults to 8081).
+//	-topic
+//	  Topic to receive incoming messages from (required).
+//	-port
+//	  Port the server is running on (optional, defaults to 8081).
 package main
 
 import (
-  "encoding/json"
-  "flag"
-  "fmt"
-  "github.com/gorilla/websocket"
-  "net/url"
-  "os"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/url"
+	"os"
 )
 
 func main() {
-  var topic string
-  flag.StringVar(&topic, "topic", "", "Topic for subscription")
+	var topic string
+	flag.StringVar(&topic, "topic", "", "Topic for subscription")
 
-  var port string
-  flag.StringVar(&port, "port", "8081", "Optionally provide the server port")
-  flag.Parse()
+	var port string
+	flag.StringVar(&port, "port", "8081", "Optionally provide the server port")
+	flag.Parse()
 
-  if topic == "" {
-     fmt.Println("Topic required to set up subscription. Exiting")
-     return
-  }
+	if topic == "" {
+		fmt.Println("Topic required to set up subscription. Exiting")
+		return
+	}
 
-  u := url.URL{Scheme: "ws", Host: "localhost:" + port, Path: "/subscribe", RawQuery: "topic=" + topic}
-  fmt.Printf("Connecting to %s\n", u.String())
+	u := url.URL{Scheme: "ws", Host: "localhost:" + port, Path: "/subscribe", RawQuery: "topic=" + topic}
+	fmt.Printf("Connecting to %s\n", u.String())
 
-  // POST? https://github.com/gorilla/websocket/issues/689
-  c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-  if err != nil {
-    fmt.Println("Dial:", err)
-    os.Exit(1)
-  }
-  defer c.Close()
+	// POST? https://github.com/gorilla/websocket/issues/689
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		fmt.Println("Dial:", err)
+		os.Exit(1)
+	}
+	defer c.Close()
 
-  // receive JSON messages
-  for {
-    var jsonMap interface{}
-    err := c.ReadJSON(&jsonMap)
-    if err != nil {
-      fmt.Println("JSON error:", err)
-    }
+	// receive JSON messages
+	for {
+		var jsonMap interface{}
+		err := c.ReadJSON(&jsonMap)
+		if err != nil {
+			fmt.Println("JSON error:", err)
+		}
 
-    dump(jsonMap)
-  }
+		dump(jsonMap)
+	}
 }
 
-func dump(data interface{}){
-  jsonBytes, err:=json.Marshal(data)
-  if err != nil {
-    fmt.Println("Error marshalling previously valid JSON, hmm...:", err)
-  }
-  fmt.Println(string(jsonBytes))
+func dump(data interface{}) {
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error marshalling previously valid JSON, hmm...:", err)
+	}
+	fmt.Println(string(jsonBytes))
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,20 +4,20 @@
 //
 // Usage:
 //
-//   go run cmd/server/main.go [-port]
+//	go run cmd/server/main.go [-port]
 //
 // The port flag is optional and defaults to 8081.
 package main
 
 import (
-  "encoding/json"
-  "flag"
-  "fmt"
-  "github.com/gorilla/websocket"
-  "io/ioutil"
-  "net/http"
-  "os"
-  "sync"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"sync"
 )
 
 // Thread-safe map of topics and an array of subscribed clients.
@@ -28,129 +28,129 @@ var topicsAndClients sync.Map
 // This is accomplished by adding the topic, client to the topicsAndClients map.
 // Clients are only subscribed to one topic at a time.
 func subscribe(w http.ResponseWriter, r *http.Request) {
-  topic := r.URL.Query().Get("topic")
-  if !(len(topic) > 0) {
-    err := "No topic specified, unable to subscribe"
-    fmt.Println(err)
-    http.Error(w, err,  http.StatusBadRequest)
-    return
-  }
+	topic := r.URL.Query().Get("topic")
+	if !(len(topic) > 0) {
+		err := "No topic specified, unable to subscribe"
+		fmt.Println(err)
+		http.Error(w, err, http.StatusBadRequest)
+		return
+	}
 
-  u := websocket.Upgrader{
-    ReadBufferSize:  1024,
-    WriteBufferSize: 1024,
-  }
-  c, err := u.Upgrade(w, r, nil)
-  if err != nil {
-    fmt.Println("Failed to establish connection: ", err)
-    return
-  }
+	u := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	c, err := u.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Failed to establish connection: ", err)
+		return
+	}
 
-  // get existing clients list for the topic or create a new one
-  clients, topicFound := topicsAndClients.Load(topic)
-  var clientsSlice []*websocket.Conn
-  if topicFound {
-    // sync.Map returns type 'any', convert to slice to enable append
-    clientsSlice = clients.([]*websocket.Conn)
-  }
+	// get existing clients list for the topic or create a new one
+	clients, topicFound := topicsAndClients.Load(topic)
+	var clientsSlice []*websocket.Conn
+	if topicFound {
+		// sync.Map returns type 'any', convert to slice to enable append
+		clientsSlice = clients.([]*websocket.Conn)
+	}
 
-  clientsSlice = append(clientsSlice, c)
-  topicsAndClients.Store(topic, clientsSlice)
+	clientsSlice = append(clientsSlice, c)
+	topicsAndClients.Store(topic, clientsSlice)
 
-  // TODO: logging, debug level
-  // fmt.Printf("Topic %s currently serving %d connections\n", topic, len(clientsSlice))
+	// TODO: logging, debug level
+	// fmt.Printf("Topic %s currently serving %d connections\n", topic, len(clientsSlice))
 
-  if err = c.WriteJSON(map[string]string{"subscription_status":"OK", "topic":topic}); err != nil {
-    fmt.Println("Subscription confirmation failed, closing connection")
-    c.Close()
-  }
+	if err = c.WriteJSON(map[string]string{"subscription_status": "OK", "topic": topic}); err != nil {
+		fmt.Println("Subscription confirmation failed, closing connection")
+		c.Close()
+	}
 }
 
 // Publish is an http handler that sends JSON data in the incoming request to all connected clients for the topic specified in the `topic` query parameter.
 // The topic must exist in the topicsAndClients map.
 // The publisher itself doesn't need to be subscribed to the topic.
 func publish(w http.ResponseWriter, r *http.Request) {
-  if r.Method != http.MethodPost {
-    err := r.Method + " not allowed for " + "/publish"
-    fmt.Println(err)
-    http.Error(w, err,  http.StatusMethodNotAllowed)
-    return
-  }
+	if r.Method != http.MethodPost {
+		err := r.Method + " not allowed for " + "/publish"
+		fmt.Println(err)
+		http.Error(w, err, http.StatusMethodNotAllowed)
+		return
+	}
 
-  topic := r.URL.Query().Get("topic")
-  if !(len(topic) > 0) {
-    err := "No topic specified, unable to publish"
-    fmt.Println(err)
-    http.Error(w, err, http.StatusBadRequest)
-    return
-  }
+	topic := r.URL.Query().Get("topic")
+	if !(len(topic) > 0) {
+		err := "No topic specified, unable to publish"
+		fmt.Println(err)
+		http.Error(w, err, http.StatusBadRequest)
+		return
+	}
 
-  contentType := r.Header.Get("Content-Type")
-  if contentType != "application/json" {
-    err := "Invalid content type"
-    fmt.Println(err)
-    http.Error(w, err, http.StatusBadRequest)
-    return
-  }
+	contentType := r.Header.Get("Content-Type")
+	if contentType != "application/json" {
+		err := "Invalid content type"
+		fmt.Println(err)
+		http.Error(w, err, http.StatusBadRequest)
+		return
+	}
 
-  body, err := ioutil.ReadAll(r.Body)
-  if err != nil {
-    fmt.Println("Failed to read request body")
-    http.Error(w, "Failed to read request body", http.StatusBadRequest)
-    return
-  } else if string(body) == "" {
-    fmt.Println("Empty body, unable to publish")
-    http.Error(w, "Empty body, unable to publish", http.StatusBadRequest)
-    return
-  }
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Failed to read request body")
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+		return
+	} else if string(body) == "" {
+		fmt.Println("Empty body, unable to publish")
+		http.Error(w, "Empty body, unable to publish", http.StatusBadRequest)
+		return
+	}
 
-  var jsonMap map[string]interface{}
-  err = json.Unmarshal([]byte(body), &jsonMap)
-  if err != nil {
-    fmt.Printf("Failed to convert \"%s\" to JSON\n", body)
-    http.Error(w, "Invalid JSON: " + string(body), http.StatusBadRequest)
-    return
-  }
+	var jsonMap map[string]interface{}
+	err = json.Unmarshal([]byte(body), &jsonMap)
+	if err != nil {
+		fmt.Printf("Failed to convert \"%s\" to JSON\n", body)
+		http.Error(w, "Invalid JSON: "+string(body), http.StatusBadRequest)
+		return
+	}
 
-  // verify topic exists, get subscribed clients
-  clients, topicFound := topicsAndClients.Load(topic)
-  if !topicFound {
-    http.Error(w, "Topic \"" + topic + "\" doesn't exist, unable to publish", http.StatusBadRequest)
-    return
-  }
+	// verify topic exists, get subscribed clients
+	clients, topicFound := topicsAndClients.Load(topic)
+	if !topicFound {
+		http.Error(w, "Topic \""+topic+"\" doesn't exist, unable to publish", http.StatusBadRequest)
+		return
+	}
 
-  // sync.Map returns type 'any', convert to slice to enable indexing
-  var clientsSlice []*websocket.Conn
-  clientsSlice = clients.([]*websocket.Conn)
+	// sync.Map returns type 'any', convert to slice to enable indexing
+	var clientsSlice []*websocket.Conn
+	clientsSlice = clients.([]*websocket.Conn)
 
-  // publish to all clients subscribed to the topic
-  clientsCopy := clientsSlice[:0]
-  for _, client := range clientsSlice {
-    if err = client.WriteJSON(jsonMap); err == nil {
-      clientsCopy = append(clientsCopy, client)
-    } else {
-      // client disconnect detected after two failed write attempts
-      fmt.Println(err)
-    }
-  }
+	// publish to all clients subscribed to the topic
+	clientsCopy := clientsSlice[:0]
+	for _, client := range clientsSlice {
+		if err = client.WriteJSON(jsonMap); err == nil {
+			clientsCopy = append(clientsCopy, client)
+		} else {
+			// client disconnect detected after two failed write attempts
+			fmt.Println(err)
+		}
+	}
 
-  topicsAndClients.Store(topic, clientsCopy)
+	topicsAndClients.Store(topic, clientsCopy)
 }
 
 func main() {
-  var port string
-  flag.StringVar(&port, "port", "8081", "Optionally provide the server port")
-  flag.Parse()
+	var port string
+	flag.StringVar(&port, "port", "8081", "Optionally provide the server port")
+	flag.Parse()
 
-  mux := http.NewServeMux()
-  mux.HandleFunc("/publish", publish)
-  mux.HandleFunc("/subscribe", subscribe)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/publish", publish)
+	mux.HandleFunc("/subscribe", subscribe)
 
-  fmt.Printf("Starting server on port %s\n", port)
-  err := http.ListenAndServe(":" + port, mux)
+	fmt.Printf("Starting server on port %s\n", port)
+	err := http.ListenAndServe(":"+port, mux)
 
-  if err != nil {
-    fmt.Printf("Server error: %s\n", err)
-    os.Exit(1)
-  }
+	if err != nil {
+		fmt.Printf("Server error: %s\n", err)
+		os.Exit(1)
+	}
 }
